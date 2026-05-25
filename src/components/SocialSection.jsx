40cd99b1cd3b98df020bdeff5de2504ed11159a6
Tanay Trivedi <135436627+TanayTrivedi2003@users.noreply.github.com>
2026-05-25 09:40:44 +0530

    Update SocialSection.jsx

--- src/components/SocialSection.jsx
+++ src/components/SocialSection.jsx
@@ -238,7 +238,7 @@ export default function SocialSection() {
     }
 
     if (num >= 1000000) {
-      return (num / 1000000).toFixed(0) + "M+";
+      return (num / 1000000).toFixed(0) + "M";
     }
 
     if (num >= 1000) {
